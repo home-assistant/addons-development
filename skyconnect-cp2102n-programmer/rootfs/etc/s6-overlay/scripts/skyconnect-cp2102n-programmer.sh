7d3a16f6e3ec1c73f340d6beb762abd26d41cab5
puddly <32534428+puddly@users.noreply.github.com>
2023-11-28 10:43:09 -0500

    Address linter issues

--- skyconnect-cp2102n-programmer/rootfs/etc/s6-overlay/scripts/skyconnect-cp2102n-programmer.sh
+++ skyconnect-cp2102n-programmer/rootfs/etc/s6-overlay/scripts/skyconnect-cp2102n-programmer.sh
@@ -4,12 +4,12 @@ set -e
 declare device
 declare device_usb_path
 declare device_attrs
-declare device_bus_num
-declare device_dev_num
+declare device_busnum
+declare device_devnum
 
 device=$(bashio::config 'device')
-device_usb_path=$(udevadm info -q path -n $device)
-device_attrs=$(udevadm info -q property -a -p $device_usb_path | grep -F 'ATTRS{')
+device_usb_path=$(udevadm info -q path -n "$device")
+device_attrs=$(udevadm info -q property -a -p "$device_usb_path" | grep -F 'ATTRS{')
 device_busnum=$(echo "$device_attrs" | grep busnum | head -n 1 | awk -F '"' '{ print $2 }')
 device_devnum=$(echo "$device_attrs" | grep devnum | head -n 1 | awk -F '"' '{ print $2 }')
 
