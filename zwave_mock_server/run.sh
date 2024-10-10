24f1c2a74029d9473e12c826f829733b96f6d68c
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-10-10 12:34:24 +0200

    a bit of linting

--- zwave_mock_server/run.sh
+++ zwave_mock_server/run.sh
@@ -6,19 +6,19 @@ echo "     Z-Wave JS Mock Server"
 echo "##################################"
 echo
 
-if [ -z "$( ls -p '/config' | grep -v / )" ]; then
-    # if addon_config folder is empty, copy all the default mock files
+found_file=false
+for file in /config/*; do
+  # print all the files (not directories) in the addons_config folder
+  if [ -f "$file" ]; then
+    found_file=true
+    echo "Detected mock file: $file"
+  fi
+done
+# if addon_config folder is empty, copy all the default mock files
+if ! $found_file; then
     echo
     echo "No mock files found in addons_config directory, using default files..."
     cp /default_mock_files/*.* /config
-    echo
-else
-    # print all the files (not directories) in the addons_config folder
-    echo
-    echo "Detected mock files in directory:"
-    echo
-    ls -p /config | grep -v /
-    echo
     echo
 fi
 
@@ -27,6 +27,7 @@ rm -rf /config/default_mock_files
 mv -f /default_mock_files /config
 
 # send discovery info to Home Assistant
+echo
 declare ha_config
 ha_config=$(\
     bashio::var.json \
@@ -41,7 +42,8 @@ fi
 
 
 # start mock driver + zwave-server
-export NODE_PATH=$(npm root --quiet -g)
+NODE_PATH="$( npm root --quiet -g )"
+export NODE_PATH
 bashio::log.info ""
 bashio::log.info "Starting Z-Wave JS Mock Driver + Z-Wave Server..."
 node /usr/local/bin/mock-server -c /config & \
